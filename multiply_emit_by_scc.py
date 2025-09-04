7a880f75f5249280140f61c5e63f2c2b436cbf74
Sandra Sattler <ssattler@ucs.org>
2025-09-04 16:06:00 -0400

    updated scc

--- multiply_emit_by_scc.py
+++ multiply_emit_by_scc.py
@@ -4,28 +4,34 @@ from os import listdir
 from glob import glob
 
 if __name__ == "__main__":
-    test_year = 2020
+
+    scenarios = listdir('results/fy25')
+    files_list = glob('results/fy25/*/emit_irt.csv')
+
+    scc = pd.read_excel("data/scc_mult.xlsx")
+    scc['eall'] = scc['eall'].ffill().str.upper()
 
 
-# Read the SCC multipliers
-scc_mult = pd.read_excel('scc.xlsx')
+    for i, scenario in enumerate(scenarios):
+        emit_irt_path = Path(f'results/fy25/{scenario}/emit_irt.csv')
+        try:
+            temp_file = pd.read_csv(emit_irt_path)
+        except FileNotFoundError:
+            print(f"File not found: {emit_irt_path}")
+            continue
+        df = temp_file.drop(columns=['i']).groupby(['eall', 'r','t']).sum().reset_index()
 
-# Ensure column names are consistent (case-insensitive match)
-emit_r.columns = emit_r.columns.str.lower()
-scc_mult.columns = scc_mult.columns.str.lower()
+        # Merge on 'eall' and 't' columns
+        merged = pd.merge(
+            df,
+            scc,
+            on=['eall', 't'],
+            suffixes=('_emit', '_scc')
+        )
+        # Multiply the values
+        merged['product'] = merged['Value_emit'] * merged['Value_scc']
+        # Output the result
+        scc_path = Path(f'results/fy25/{scenario}/scc_values.csv')
+        merged.to_csv(scc_path, index=False)
 
-# Merge on 'eall' and 't' columns
-merged = pd.merge(
-    emit_r,
-    scc_mult,
-    on=['eall', 't'],
-    suffixes=('_emit', '_scc')
-)
 
-# Multiply the values
-merged['product'] = merged['value_emit'] * merged['value_scc']
-
-# Output the result
-merged.to_csv('scc_values.csv', index=False)
-
-print('Multiplication complete. Output saved to scc_values.csv')
