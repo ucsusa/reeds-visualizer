27d9601dbce3b41b0517185d4bc87957114d21b0
Sam Dotson <samgdotson@gmail.com>
2025-09-05 12:58:59 -0400

    deflates costs to 2004$

--- multiply_emit_by_scc.py
+++ multiply_emit_by_scc.py
@@ -4,9 +4,14 @@ from os import listdir
 from glob import glob
 
 if __name__ == "__main__":
-
+    dollar_year = 2020
     scenarios = listdir('results/fy25')
     scc = pd.read_csv("data/scc_mult.csv")
+    deflator = pd.read_csv(("https://raw.githubusercontent.com/"
+                            "ucsusa/ReEDS-2.0/"
+                            "0b57ae128bf9295dee0cf0218fe5f5fc800d26d9/"
+                            "inputs/financials/deflator.csv"), 
+                            index_col="*Dollar.Year")
 
     for i, scenario in enumerate(scenarios):
         emit_irt_path = Path(f'results/fy25/{scenario}/emit_irt.csv')
@@ -27,6 +32,10 @@ if __name__ == "__main__":
         # Multiply the values
         merged['Value'] = merged['Value_emit'] * merged['Value_scc']
         merged = merged.drop(columns=['Value_emit','Value_scc'])
+        
+        # Deflate to 2004$
+        merged['Value'] = merged['Value'] * deflator.at[dollar_year, 'Deflator']
+        
         # Output the result
         scc_path = Path(f'results/fy25/{scenario}/scc_values.csv')
         merged.to_csv(scc_path, index=False)
