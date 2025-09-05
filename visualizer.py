d3a9b623fade26af21417ddb12faa53dbe190c02
Sam Dotson <samgdotson@gmail.com>
2025-09-05 09:29:43 -0400

    last one??

--- visualizer.py
+++ visualizer.py
@@ -92,7 +92,7 @@ def get_data(metric):
         full_df = full_df.groupby(cols).sum().drop(columns=['r']).reset_index()
     return full_df
 
-
+ 
 # You should cache your pygwalker renderer, if you don't want your memory to explode 
 # @st.cache_resource
 def get_pyg_renderer() -> "StreamlitRenderer":
